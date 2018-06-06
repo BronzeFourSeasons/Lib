d361f481cf2d26d8c78722031e6078ecbacbbe5e
QianFeng <1247997687@qq.com>
2018-06-06 17:49:10 +0800

    Update StringT.java

--- lib/src/main/java/com/whf/android/jar/StringT.java
+++ lib/src/main/java/com/whf/android/jar/StringT.java
@@ -1,1 +1,14 @@
+package com.whf.android.jar;
 
+
+
+/**
+ * Print related settings page
+ *
+ * @author qf
+ * @author wang.hai.fang
+ * @since 2.5.0
+ */
+public final class StringT {
+    public static final String APK_TYPE = "application/vnd.android.package-archive";
+}
