5e8be5c36a730905543ffb6ba50f085f31c65c9f
QianFeng <1247997687@qq.com>
2018-08-08 11:17:41 +0800

    Update DateTimePop.java

--- lib/src/main/java/com/whf/android/jar/popup/DateTimePop.java
+++ lib/src/main/java/com/whf/android/jar/popup/DateTimePop.java
@@ -44,7 +44,7 @@ public class DateTimePop extends PopupWindow implements CalendarView.OnDateChang
     private ChangeNoViewPager mViewPager;
     private LayoutInflater inflater;
     private String strDate, strTime;
-    private IListener iListener;
+    private OnDateTimeListener iListener;
     private final static String[] titles = {"日期", "时间"};
     public final static int START_TIME = 11,END_TIME = 12,APPLY_TIME = 13,USE_TIME = 14;
 
