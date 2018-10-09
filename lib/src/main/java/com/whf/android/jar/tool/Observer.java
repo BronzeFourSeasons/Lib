4bb908494b823700e7f20e707506f3b20a1d5747
QianFeng <1247997687@qq.com>
2018-10-09 11:32:27 +0800

    请求异常

--- lib/src/main/java/com/whf/android/jar/tool/Observer.java
+++ lib/src/main/java/com/whf/android/jar/tool/Observer.java
@@ -19,19 +19,34 @@ public abstract class Observer<T> implements io.reactivex.Observer<T> {
 
     @Override
     public void onError(Throwable e) {
+        LogT.e(e.getMessage());
         if (e instanceof HttpException) {
             HttpException httpException = (HttpException) e;
             int code = httpException.code();
-            if (code == 404 || code == 500) {
-                onError("服务器错误，请稍后重试");
+            if (code == 404) {
+                 onError("服务器无回应");
+            } else if (code == 500) {
+                 onError("服务器错误");
             }
         } else if (e instanceof ConnectException) {
             onError(NetT.isConnected(BaseApplication.getContext()) ? "连接不到服务器，请稍后重试" : "连接不到服务器，请检查网路");
         } else if (e instanceof SocketException) {
+            onError("连接服务器错误，请稍后重试");
+        } else if (e instanceof SocketTimeoutException) {
             onError("连接服务器超时，请稍后重试");
         } else {
             onError("未知错误:" + e.getMessage());
         }
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
     }
 
 }
