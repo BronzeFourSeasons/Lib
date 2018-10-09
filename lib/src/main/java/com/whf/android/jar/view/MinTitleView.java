1fe4dca13401e709667a29eadab05b0b43fd2822
QianFeng <1247997687@qq.com>
2018-10-09 14:05:17 +0800

    标题

--- lib/src/main/java/com/whf/android/jar/view/MinTitleView.java
+++ lib/src/main/java/com/whf/android/jar/view/MinTitleView.java
@@ -1,5 +1,6 @@
 package com.whf.android.jar.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -10,6 +11,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.whf.android.jar.BarColorT;
+import com.whf.android.jar.LogT;
 import com.whf.android.jar.R;
 
 /**
@@ -21,9 +23,10 @@ import com.whf.android.jar.R;
  */
 public class MinTitleView extends LinearLayout {
 
-    private TextView text,textBack;
+    private TextView text, textBack;
     private Context context;
     private AttributeSet attrs;
+    private View.OnClickListener onClickListener;
 
     public MinTitleView(Context context) {
         super(context);
@@ -44,20 +47,41 @@ public class MinTitleView extends LinearLayout {
      */
     private void initView() {
         LayoutInflater.from(context).inflate(R.layout.hai_layout_min_title, this, true);
-        text = (TextView) findViewById(R.id.id_textTitle);
-        textBack = (TextView)findViewById(R.id.id_textBack);
+        text = getView(R.id.id_textTitle);
+        textBack = getView(R.id.id_textBack);
         findViewById(R.id.id_return).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (onClickListener != null) {
+                    onClickListener.onClick(v);
+                }
                 ((Activity) getContext()).finish();
             }
         });
+    }
+
+    public void setOnClickListener(View.OnClickListener l) {
+        onClickListener = l;
+    }
+
+    /**
+     * 通用findViewById,减少重复的类型转换
+     */
+    @SuppressWarnings("unchecked")
+    public final <E extends View> E getView(int id) {
+        try {
+            return (E) findViewById(id);
+        } catch (ClassCastException ex) {
+            LogT.e(ex.getMessage());
+            throw ex;
+        }
     }
 
     /**
      * init
      */
     private void onTypedArray() {
+        @SuppressLint("Recycle")
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MinTitleView);
         String title = a.getString(R.styleable.MinTitleView_text);
         int titleColor = a.getColor(R.styleable.MinTitleView_color, 0xff1e90ff);
