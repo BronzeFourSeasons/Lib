1c04409a3793ed423d129b76c360b350b3293a98
QianFeng <1247997687@qq.com>
2018-10-10 11:32:25 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/cqtk/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cqtk.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
