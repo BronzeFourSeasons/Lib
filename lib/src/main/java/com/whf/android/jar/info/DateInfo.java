b3ed732fe15cefbe7a6869d9095cb105684b62fd
QianFeng <1247997687@qq.com>
2018-11-29 15:50:31 +0800

    三个参数

---
+++ lib/src/main/java/com/whf/android/jar/info/DateInfo.java
@@ -0,0 +1,46 @@
+package com.whf.android.jar.info;
+
+/**
+ *
+ * @author 千凨
+ * @date 2018/11/29
+ */
+
+public class DateInfo{
+        /**
+         * 标题
+         */
+        private String title;
+        /**
+         * 内容
+         */
+        private String content;
+        /**
+         * 备注
+         */
+        private String remarks;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getRemarks() {
+            return remarks;
+        }
+
+        public void setRemarks(String remarks) {
+            this.remarks = remarks;
+        }
+}
