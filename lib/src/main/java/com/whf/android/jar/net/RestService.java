4250ec192f33a059410eb23a4eeafc9078d4f857
QianFeng <1247997687@qq.com>
2019-03-11 15:45:13 +0800

    baseUrl切换

---
+++ lib/src/main/java/com/whf/android/jar/net/RestService.java
@@ -0,0 +1,25 @@
+package com.whf.android.jar.net;
+
+/**
+ * baseUrl切换
+ *
+ * Created by 傅令杰
+ * @author 傅令杰
+ */
+public interface RestService {
+    /**
+     * 更换接口的key
+     */
+    String BASE_URL = "url_name";
+
+    /**
+     * 更换接口的v
+     */
+    String OTHER = "other";
+
+    /**
+     * 更换接口的v
+     */
+    String BASE_V = "BaiDu";
+
+}
